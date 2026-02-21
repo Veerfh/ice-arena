c1195e06e200cafbe39b3a4eee428f8efc881eae
Veerfh <veeerfh@gmail.com>
2026-02-21 10:11:31 +0500

    Маршруты

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\TicketController;
+use App\Http\Controllers\BookingController;
+use App\Http\Controllers\PaymentController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+Route::post('/ticket/purchase', [TicketController::class, 'store'])->name('ticket.purchase');
+Route::post('/booking/create', [BookingController::class, 'store'])->name('booking.create');
+
+Route::get('/payment/success', [PaymentController::class, 'success'])->name('payment.success');
+Route::post('/payment/webhook', [PaymentController::class, 'webhook'])->name('payment.webhook');
